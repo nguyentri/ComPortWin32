fbcdfa55a2670f34dc55091511538bf526e10250
nguyentri <trongtribk06@gmail.com>
2014-07-05 23:01:09 +0700

    Change Openning the comport

--- Serial_Comunication/Program.cs
+++ Serial_Comunication/Program.cs
@@ -16,17 +16,7 @@ namespace Serial_Comunication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-
-           
-            
-
-
-
-
-
+            Application.Run(new DataLog());
         }
-
-
     }
 }
