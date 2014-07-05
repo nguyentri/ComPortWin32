d78c0a9f00477c7e84beb48bb6e5af7390ff3884
nguyentri <trongtribk06@gmail.com>
2014-07-05 14:25:06 +0700

    Initial version

---
+++ Serial_Comunication/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;  //add this,represents a serial port resource
+
+
+namespace Serial_Comunication
+{
+    public partial class Form1 : Form
+    {
+        SerialPort myserial = new SerialPort(); //create of serial port
+
+        public Form1()
+        { 
+            InitializeComponent();
+            getportnames();                   //load all port names in this computer
+            comboBoxBaudR.SelectedIndex = 6;  //default  set for baudrate-9600
+            comboBoxDataB.SelectedIndex = 1;  //default  set for data bits-8
+            comboBoxParity.SelectedIndex = 0; //default  set for parity -none
+            comboBoxStopB.SelectedIndex = 0;  //default  set for stop bits -one
+          
+        }
+
+      
+        public void Init()
+        {
+           if (myserial.IsOpen )  //if port is  open 
+            { myserial.Close();  //close port
+            }
+          myserial.PortName = comboBoxPort.Text;                                           //selected name of port
+          myserial.Parity     = (Parity)Enum.Parse(typeof(Parity), comboBoxParity.Text);   //selected parity 
+          myserial.StopBits   = (StopBits)Enum.Parse(typeof(StopBits), comboBoxStopB.Text);//selected stopbits
+          myserial.DataBits   =   int.Parse(comboBoxDataB.Text);                           //selected data bits
+          myserial.BaudRate   = int.Parse(comboBoxBaudR.Text);                             //selected baudrate
+          myserial.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);//received even handler
+        }
+        public void getportnames()         
+        {
+            string[] portnames = SerialPort.GetPortNames(); //load all names of  com ports to string 
+            comboBoxPort.Items.Clear();                     //delete previous names in combobox items 
+            
+            foreach (string s in portnames)                 //add this names to comboboxPort items
+            { comboBoxPort.Items.Add(s);
+            }
+
+           if (comboBoxPort.Items.Count > 0)   //if there are some com ports ,select first 
+            { comboBoxPort.SelectedIndex = 0;
+            }
+           else
+            { comboBoxPort.Text = "No COM Port "; //if there are no com ports ,write No Com Port
+            }
+        }
+        public  void  transmit()    
+        {
+            Init();                 //init parameters of serial comunication before every touch button "send"
+            try
+            {
+                myserial.Open();        //open serial port
+                myserial.Write(richTextBoxSend.Text); //write string  to serial port from rich text box 
+            }
+            catch
+            {
+                MessageBox.Show("com port is not available"); //if there are not is any COM port in PC show message
+            }
+        }
+
+        public void DataReceivedHandler( object sender,SerialDataReceivedEventArgs e)
+        {
+          string indata=  myserial.ReadExisting();  //read data to string 
+          Invoke(new Action(() => richTextBoxReceive.Text += indata)); //invoke method use for write receive data to richtextbox 
+        }
+        private void buttonSend_Click(object sender, EventArgs e) // send button  event
+        { transmit();     //transmit data
+        }
+        private void buttonReload_Click(object sender, EventArgs e)//reload button event ,most useful if you use virtual COM port e.g FTDI,Prolific 
+        { getportnames(); //get all port names connected to this computer
+        }
+
+       
+    }
+}
---
+++ Serial_Comunication/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serial_Comunication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Serial_Comunication")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35801a52-a0c6-4c54-870d-942f5a2a0404")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
